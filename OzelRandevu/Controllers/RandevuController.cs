8d9682d738ab7087b0013a4f877aceef0717102f
İsmail Çendik <ismailcendik493@gmail.com>
2022-03-25 17:45:34 +0300

    Randevu Controller eklendi

---
+++ OzelRandevu/Controllers/RandevuController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OzelRandevu.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OzelRandevu.Controllers
+{
+    public class RandevuController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RandevuController(ApplicationDbContext context)
+        {
+
+            _context = context;
+
+        }
+
+        public JsonResult GetRandevu()
+        {
+            var model = _context.OzelRandevular        /* sorgulamaya gore randevuları tekrar listeleyebiliriz*/
+                .Include(x=>x.User).Select(x=>new RandevuViewModel()
+                    { 
+                        Doktor = x.User.Ad + " " + x.User.Soyad,
+                        Hasta = x.HastaAdi + " "+ x.HastaSoyadi,
+                        BaslangicTarihi = x.BaslangicTarihi,
+                        BitisTarihi=x.BitisTarihi,
+                        Aciklama=x.Aciklama,
+                        UserId = x.UserId
+
+            
+                    });  
+            return Json(model);
+        }
+
+
+        public JsonResult GetRandevu(string userId="" )
+        {
+            var model = _context.OzelRandevular.Where(x=>x.UserId==userId)        
+                .Include(x => x.User).Select(x => new RandevuViewModel()
+                {
+                    Doktor = x.User.Ad + " " + x.User.Soyad,
+                    Hasta = x.HastaAdi + " " + x.HastaSoyadi,
+                    BaslangicTarihi = x.BaslangicTarihi,
+                    BitisTarihi = x.BitisTarihi,
+                    Aciklama = x.Aciklama,
+                    UserId = x.UserId
+
+
+                });
+            return Json(model);
+        }
+
+
+    }
+}
---
+++ OzelRandevu/Models/RandevuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OzelRandevu
+{
+    public class RandevuViewModel
+    {
+        public RandevuViewModel()
+        {
+        }
+
+        public string Doktor { get; internal set; }
+        public string Hasta { get; internal set; }
+        public DateTime BaslangicTarihi { get; internal set; }
+        public DateTime BitisTarihi { get; internal set; }
+        public string Aciklama { get; internal set; }
+        public string UserId { get; internal set; }
+    }
+}
