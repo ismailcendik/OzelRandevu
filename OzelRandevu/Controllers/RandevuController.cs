cfb815c5f5d1b4fbe493fc455b07ecbe4b023616
İsmail Çendik <ismailcendik493@gmail.com>
2022-03-31 16:22:54 +0300

    Randevu Controller ve Model oluşturuldu

--- OzelRandevu/Controllers/RandevuController.cs
+++ OzelRandevu/Controllers/RandevuController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OzelRandevu.Data;
+using OzelRandevu.Data.Entity;
+using OzelRandevu.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,6 +56,62 @@ namespace OzelRandevu.Controllers
             return Json(model);
         }
 
+        [HttpPost]
+
+        public JsonResult RandevuEkleGuncelle(RandevuEkleGuncelleModel model)
+        {
+            //Basit validasyon   31.03 İsmail Çendik
+
+            if (model.Id==0)
+            {
+                Ozel_Randevu entity = new Ozel_Randevu()
+                {
+                    OlusturmaTarihi = DateTime.Now,
+                    BaslangicTarihi = model.BaslangicTarihi,
+                    BitisTarihi = model.BitisTarihi,
+                    HastaAdi = model.HastaAdi,
+                    HastaSoyadi = model.HastaSoyadi,
+                    Aciklama = model.Aciklama,
+                    UserId = model.UserId
+                };
+
+                _context.Add(entity);
+                _context.SaveChanges();
+            }
+            else
+            {
+                var entity = _context.OzelRandevular.SingleOrDefault(x => x.Id == model.Id);
+                if (entity==null)
+                {
+                    return Json("Güncellenecek veri bulunamadı");
+                }
+                entity.GuncellemeTarihi = DateTime.Now;
+                entity.HastaAdi = model.HastaAdi;
+                entity.HastaSoyadi = model.HastaSoyadi;
+                entity.Aciklama = model.Aciklama;
+                entity.BaslangicTarihi = model.BaslangicTarihi;
+                entity.BitisTarihi = model.BitisTarihi;
+                entity.UserId = model.UserId;
+
+                _context.Update(entity);
+                _context.SaveChanges();
+            }
+            return Json("200");
+
+        }
+
+        public JsonResult RandevuSil(int id = 0)
+        {
+            var entity = _context.OzelRandevular.SingleOrDefault(x => x.Id == id);
+            if (entity==null)
+            {
+                return Json("Kayıt bulunamadı");
+            }
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return Json("200");
+        }
 
     }
+    
 }
---
+++ OzelRandevu/Models/RandevuEkleGuncelleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OzelRandevu.Models
+{
+    public class RandevuEkleGuncelleModel
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime BaslangicTarihi { get; set; }
+
+        public DateTime BitisTarihi { get; set; }
+
+        public string HastaAdi { get; set; }
+
+        public string HastaSoyadi { get; set; }
+
+        public string Aciklama { get; set; }
+    }
+}
